96ae2041d4e3b4df4866aa145192ac9266d7aa69
prasum shrestha <prasumshrestha8877@gmail.com>
2026-04-13 17:41:02 +0545

    glass fill detection and game manager

--- Assets/Script/LineDrawingController.cs
+++ Assets/Script/LineDrawingController.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Numerics;
-using System.Threading.Tasks.Dataflow;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -10,27 +7,31 @@ using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 #endif
 
-public class LineDrawingController:MonoBehaviour
+public class LineDrawingController : MonoBehaviour
 {
+    [Header("Line Look")]
     [SerializeField] private Material lineMaterial;
     [SerializeField] private float lineWidth = 0.14f;
-    [SerializeField] private Color lineColor = Color.Black;
-    [SerializeField] private int lineSortingOrder= 5;
+    [SerializeField] private Color lineColor = Color.black;
+    [SerializeField] private int lineSortingOrder = 5;
 
+    [Header("Line Shape")]
     [SerializeField] private float minimumDistanceBetweenPoints = 0.08f;
     [SerializeField] private float maximumLineLength = 8f;
     [SerializeField] private float colliderThickness = 0.14f;
 
-    [SerializeField] private RigidBody2D lineBodyTypeAfterRelease = RigidBodyType2D.Dynamic;
-    [SerializeField] private float lineGravityAfterRelease =0f;
+    [Header("Line Rigidbody After Release")]
+    [SerializeField] private RigidbodyType2D lineBodyTypeAfterRelease = RigidbodyType2D.Static;
+    [SerializeField] private float lineGravityAfterRelease = 0f;
     [SerializeField] private bool freezeLineRotationAfterRelease = true;
 
+    [Header("Drawing Rules")]
     [SerializeField] private int maximumLinesPerRound = 1;
-    [SerializeField] private bool lockDrawingAfterLinelimit = true;
+    [SerializeField] private bool lockDrawingAfterLineLimit = true;
 
     private readonly List<Vector2> lineWorldPoints = new List<Vector2>();
 
-        private Camera cachedMainCamera;
+    private Camera cachedMainCamera;
     private Material runtimeLineMaterial;
 
     private GameObject currentLineObject;
@@ -50,57 +51,55 @@ public class LineDrawingController:MonoBehaviour
 
     private void Awake()
     {
-        cachedMainCamera= Camera.main;
+        cachedMainCamera = Camera.main;
     }
 
     private void Update()
     {
-        if(!drawingAllowed)
+        if (!drawingAllowed)
         {
             return;
         }
 
-        if(TryGetPointerDown(out Vector2 downScreenPosition))
+        if (TryGetPointerDown(out Vector2 downScreenPosition))
         {
-            if(!IsPointerOnUI())
+            if (!IsPointerOnUI())
             {
                 Vector2 startWorldPoint = ConvertScreenToWorld(downScreenPosition);
-
                 StartNewLine(startWorldPoint);
             }
         }
 
-        if(!isCurrentlyDrawing)
+        if (!isCurrentlyDrawing)
         {
             return;
         }
 
-        if(TryGetPointerHeld(out Vector2 holdScreenPosition))
+        if (TryGetPointerHeld(out Vector2 holdScreenPosition))
         {
             Vector2 nextWorldPoint = ConvertScreenToWorld(holdScreenPosition);
-
             AddPointIfNeeded(nextWorldPoint);
         }
-        if(TryGetPointerUp(out _))
+
+        if (TryGetPointerUp(out _))
         {
             FinishCurrentLine();
         }
     }
-    
+
     public void SetDrawingAllowed(bool canDraw)
     {
         drawingAllowed = canDraw;
 
-        if(!drawingAllowed && isCurrentlyDrawing)
+        if (!drawingAllowed && isCurrentlyDrawing)
         {
             FinishCurrentLine();
         }
-
     }
 
-    public void ClearnAllDrawlines()
+    public void ClearAllDrawnLines()
     {
-        for(int i= TransformBlock.childCount-1; i>=0; i--)
+        for (int i = transform.childCount - 1; i >= 0; i--)
         {
             Destroy(transform.GetChild(i).gameObject);
         }
@@ -113,30 +112,32 @@ public class LineDrawingController:MonoBehaviour
 
         currentLineObject = null;
         currentLineRenderer = null;
+        currentLinePolygonCollider = null;
+        currentLineRigidbody = null;
     }
 
     private void StartNewLine(Vector2 startWorldPoint)
     {
-        if(isCurrentlyDrawing)
+        if (isCurrentlyDrawing)
         {
             return;
         }
 
-        if(maximumLinesPerRound>0 && finishedLineCount>= maximumLinesPerRound)
+        if (maximumLinesPerRound > 0 && finishedLineCount >= maximumLinesPerRound)
         {
             return;
         }
 
-        if(cachedMainCamera == null)
+        if (cachedMainCamera == null)
         {
-            //WHAT DOES THIS MEAN???
             cachedMainCamera = Camera.main;
-            if(cachedMainCamera == null)
+            if (cachedMainCamera == null)
             {
                 return;
             }
         }
-          currentLineObject = new GameObject("PlayerLine_" + (finishedLineCount + 1));
+
+        currentLineObject = new GameObject("PlayerLine_" + (finishedLineCount + 1));
         currentLineObject.transform.SetParent(transform, true);
 
         currentLineRigidbody = currentLineObject.AddComponent<Rigidbody2D>();
@@ -167,7 +168,6 @@ public class LineDrawingController:MonoBehaviour
         isCurrentlyDrawing = true;
 
         AddPointToCurrentLine(startWorldPoint);
-
     }
 
     private void AddPointIfNeeded(Vector2 worldPoint)
@@ -196,9 +196,7 @@ public class LineDrawingController:MonoBehaviour
         if (newSegmentLength > remainingLength)
         {
             Vector2 direction = (worldPoint - lastPoint).normalized;
-
             worldPoint = lastPoint + direction * remainingLength;
-
             newSegmentLength = remainingLength;
         }
 
@@ -209,18 +207,16 @@ public class LineDrawingController:MonoBehaviour
         {
             FinishCurrentLine();
         }
-
     }
+
     private void AddPointToCurrentLine(Vector2 worldPoint)
     {
         lineWorldPoints.Add(worldPoint);
 
-        Vector2 localPoint = currentLineObject.transform.InverseTransformPoint;
+        Vector2 localPoint = currentLineObject.transform.InverseTransformPoint(worldPoint);
 
-        int pointIndex = lineWorldPoints.Count -1;
-
+        int pointIndex = lineWorldPoints.Count - 1;
         currentLineRenderer.positionCount = lineWorldPoints.Count;
-        
         currentLineRenderer.SetPosition(pointIndex, localPoint);
 
         if (lineWorldPoints.Count < 2)
@@ -229,34 +225,32 @@ public class LineDrawingController:MonoBehaviour
         }
 
         Vector2[] localPoints = new Vector2[lineWorldPoints.Count];
-
-        for(int i=0; i < lineWorldPoints.Count; i++)
+        for (int i = 0; i < lineWorldPoints.Count; i++)
         {
-            localPoints[i] = currentLineObject.transform.InverseTransformPoint;
+            localPoints[i] = currentLineObject.transform.InverseTransformPoint(lineWorldPoints[i]);
         }
 
-        UpdatePolygonColliderFromline(localPoints);
-    }   
+        UpdatePolygonColliderFromLine(localPoints);
+    }
 
-    private voidUpdatePolygonColliderFromLine(Vector2[] localPoints)
+    private void UpdatePolygonColliderFromLine(Vector2[] localPoints)
     {
-        if(currentLinePolygonCollider == null || localPoints = null || localPoints.Length < 2)
+        if (currentLinePolygonCollider == null || localPoints == null || localPoints.Length < 2)
         {
             return;
         }
 
-        float halfThickness = MathF.Max(0,001f, colliderThickness* 0.5f);
+        float halfThickness = Mathf.Max(0.001f, colliderThickness * 0.5f);
 
         int pointCount = localPoints.Length;
-
         Vector2[] leftSidePoints = new Vector2[pointCount];
-
         Vector2[] rightSidePoints = new Vector2[pointCount];
 
-        for(int i = 0; i< pointCount; i++)
+        for (int i = 0; i < pointCount; i++)
         {
             Vector2 direction;
-             if (i == 0)
+
+            if (i == 0)
             {
                 direction = (localPoints[1] - localPoints[0]).normalized;
             }
@@ -281,7 +275,8 @@ public class LineDrawingController:MonoBehaviour
             leftSidePoints[i] = localPoints[i] + normal * halfThickness;
             rightSidePoints[i] = localPoints[i] - normal * halfThickness;
         }
-         Vector2[] polygonPath = new Vector2[pointCount * 2];
+
+        Vector2[] polygonPath = new Vector2[pointCount * 2];
         for (int i = 0; i < pointCount; i++)
         {
             polygonPath[i] = leftSidePoints[i];
@@ -294,20 +289,228 @@ public class LineDrawingController:MonoBehaviour
 
     private void FinishCurrentLine()
     {
-        if(!isCurrentlyDrawing)
+        if (!isCurrentlyDrawing)
         {
             return;
         }
 
         isCurrentlyDrawing = false;
 
-        if(lineWorldPoints.Count < 2)
+        if (lineWorldPoints.Count < 2)
         {
-            if(currentLineObject != null)
+            if (currentLineObject != null)
             {
                 Destroy(currentLineObject);
             }
-            ResetCurrentLineRefereneces();
+
+            ResetCurrentLineReferences();
+            return;
+        }
+
+        EnablePhysicsForFinishedLine();
+
+        finishedLineCount++;
+        LineFinished?.Invoke();
+
+        if (finishedLineCount == 1)
+        {
+            FirstLineFinished?.Invoke();
+        }
+
+        if (lockDrawingAfterLineLimit && maximumLinesPerRound > 0 && finishedLineCount >= maximumLinesPerRound)
+        {
+            drawingAllowed = false;
+        }
+
+        ResetCurrentLineReferences();
+    }
+
+    private void EnablePhysicsForFinishedLine()
+    {
+        if (currentLineRigidbody == null)
+        {
+            return;
+        }
+
+        currentLineRigidbody.bodyType = lineBodyTypeAfterRelease;
+        currentLineRigidbody.gravityScale = lineGravityAfterRelease;
+        currentLineRigidbody.constraints = freezeLineRotationAfterRelease
+            ? RigidbodyConstraints2D.FreezeRotation
+            : RigidbodyConstraints2D.None;
+        currentLineRigidbody.simulated = true;
+        currentLineRigidbody.WakeUp();
+    }
+
+    private void ResetCurrentLineReferences()
+    {
+        currentLineObject = null;
+        currentLineRenderer = null;
+        currentLinePolygonCollider = null;
+        currentLineRigidbody = null;
+
+        lineWorldPoints.Clear();
+        currentLineLength = 0f;
+    }
+
+    private Material GetLineMaterial()
+    {
+        if (lineMaterial != null)
+        {
+            return lineMaterial;
+        }
+
+        if (runtimeLineMaterial == null)
+        {
+            Shader spriteShader = Shader.Find("Sprites/Default");
+            if (spriteShader != null)
+            {
+                runtimeLineMaterial = new Material(spriteShader);
+            }
+        }
+
+        return runtimeLineMaterial;
+    }
+
+    private Vector2 ConvertScreenToWorld(Vector2 screenPoint)
+    {
+        if (cachedMainCamera == null)
+        {
+            cachedMainCamera = Camera.main;
+            if (cachedMainCamera == null)
+            {
+                return Vector2.zero;
+            }
+        }
+
+        Vector3 worldPoint = cachedMainCamera.ScreenToWorldPoint(
+            new Vector3(screenPoint.x, screenPoint.y, -cachedMainCamera.transform.position.z));
+
+        return new Vector2(worldPoint.x, worldPoint.y);
+    }
+
+    private bool IsPointerOnUI()
+    {
+        if (EventSystem.current == null)
+        {
+            return false;
+        }
+
+#if ENABLE_INPUT_SYSTEM
+        if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            int touchId = Touchscreen.current.primaryTouch.touchId.ReadValue();
+            return EventSystem.current.IsPointerOverGameObject(touchId);
+        }
+
+        return EventSystem.current.IsPointerOverGameObject();
+#else
+        if (Input.touchCount > 0)
+        {
+            return EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
+        }
+
+        return EventSystem.current.IsPointerOverGameObject();
+#endif
+    }
+
+    private static bool TryGetPointerDown(out Vector2 screenPoint)
+    {
+#if ENABLE_INPUT_SYSTEM
+        if (Touchscreen.current != null)
+        {
+            var touch = Touchscreen.current.primaryTouch;
+            screenPoint = touch.position.ReadValue();
+            return touch.press.wasPressedThisFrame;
+        }
+
+        if (Mouse.current != null)
+        {
+            screenPoint = Mouse.current.position.ReadValue();
+            return Mouse.current.leftButton.wasPressedThisFrame;
+        }
+
+        screenPoint = Vector2.zero;
+        return false;
+#else
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            screenPoint = touch.position;
+            return touch.phase == TouchPhase.Began;
+        }
+
+        screenPoint = Input.mousePosition;
+        return Input.GetMouseButtonDown(0);
+#endif
+    }
+
+    private static bool TryGetPointerHeld(out Vector2 screenPoint)
+    {
+#if ENABLE_INPUT_SYSTEM
+        if (Touchscreen.current != null)
+        {
+            var touch = Touchscreen.current.primaryTouch;
+            screenPoint = touch.position.ReadValue();
+            return touch.press.isPressed;
+        }
+
+        if (Mouse.current != null)
+        {
+            screenPoint = Mouse.current.position.ReadValue();
+            return Mouse.current.leftButton.isPressed;
+        }
+
+        screenPoint = Vector2.zero;
+        return false;
+#else
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            screenPoint = touch.position;
+            return touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary;
+        }
+
+        screenPoint = Input.mousePosition;
+        return Input.GetMouseButton(0);
+#endif
+    }
+
+    private static bool TryGetPointerUp(out Vector2 screenPoint)
+    {
+#if ENABLE_INPUT_SYSTEM
+        if (Touchscreen.current != null)
+        {
+            var touch = Touchscreen.current.primaryTouch;
+            screenPoint = touch.position.ReadValue();
+            return touch.press.wasReleasedThisFrame;
+        }
+
+        if (Mouse.current != null)
+        {
+            screenPoint = Mouse.current.position.ReadValue();
+            return Mouse.current.leftButton.wasReleasedThisFrame;
+        }
+
+        screenPoint = Vector2.zero;
+        return false;
+#else
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            screenPoint = touch.position;
+            return touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled;
+        }
+
+        screenPoint = Input.mousePosition;
+        return Input.GetMouseButtonUp(0);
+#endif
+    }
+
+    private void OnDestroy()
+    {
+        if (runtimeLineMaterial != null)
+        {
+            Destroy(runtimeLineMaterial);
         }
     }
 }
